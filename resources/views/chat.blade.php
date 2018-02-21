02edff823bc5f38ffd1bb461fc6376ce67570f48
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-22 02:39:22 +0800

    fixing some css in chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token()  }}">
+  <title>Chat Apps</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <style>
+      .list-group {
+          overflow-y : scroll;
+          height : 200px;
+      }
+  </style>
+</head>
+<body>
+    <div class="container">
+      <div class="row" id="app">
+        <div class="offset-4 col-4">
+          <li class="list-group-item active">Chat Room</li>
+          <ul class="list-group">
+            <message v-for="value in chat.message" > @{{value}} </message>
+          </ul>
+            <input type="text" v-model="message" @keyup.enter="send" class="form-control" placeholder="Type your message here...">
+        </div>
+      </div>
+    </div>
+    
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
