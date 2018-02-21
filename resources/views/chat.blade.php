675a24b889ef053dff41f8164ea39fe7f02bad6d
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-22 03:19:02 +0800

    customize the displays

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -18,7 +18,7 @@
         <div class="offset-4 col-4">
           <li class="list-group-item active">Chat Room</li>
           <ul class="list-group" v-chat-scroll>
-            <message v-for="value in chat.message" :key=value.index > @{{value}} </message>
+            <message v-for="value in chat.message" :key=value.index color="success" > @{{value}} </message>
           </ul>
             <input type="text" v-model="message" @keyup.enter="send" class="form-control" placeholder="Type your message here...">
         </div>
