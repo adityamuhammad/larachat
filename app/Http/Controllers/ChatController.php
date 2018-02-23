13272bd892fe87a005091927f8d24442f09b7538
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-23 16:09:12 +0800

    add middleware auth to chatcontroller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,6 +8,11 @@ use Auth;
 
 class ChatController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function chat()
     {
         return view('chat');
