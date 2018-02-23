a7aa145ea34d9fc6a6d4ee2816af5be522d64aa7
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-23 16:07:45 +0800

    add authentication scafolding laravel

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Auth;
 
 class ChatController extends Controller
 {
-    //
+    public function chat()
+    {
+        return view('chat');
+    }
+
+    public function send(Request $request)
+    {
+        $user = User::find(Auth::id());
+        event(new ChatEvent($request->message, $user));
+    }
 }
